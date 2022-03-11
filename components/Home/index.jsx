3d30814f650f85179d1100c818a56e86c80cbd2c
pc <zouiten.issam@gmail.com>
2022-03-11 01:37:54 +0100

    profile

--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -1,123 +1,172 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button, Pressable, TextInput, ImageBackground } from 'react-native';
-import axios from 'axios';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TextInput,
+  ImageBackground,
+  SafeAreaView,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import axios from "axios";
+import Home_image from "../../assets/42_logo.png";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export default function Index({ navigation }) {
-    const [login, onChangeLogin] = React.useState("");
-    const image = { uri: "https://images.unsplash.com/photo-1530176928500-2372a88e00b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fDNkJTIwYmFja2dyb3VuZCUyMHdoaXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" };
-    const getToken = async () => {
-        try {
-            const response = await axios.post(`https://api.intra.42.fr/oauth/token`,
-                {
-                    grant_type: "client_credentials",
-                    client_id: "160ec65318675dc7088c2c4b6139fbae91387ae8bcc71c363e0597676532bd70",
-                    client_secret: "21e6dfeba0b0cf4d69bd9d7516a1fafc3df090f4ba5e0538f9c6b3e258363ab0",
-                }
-            )
-            if (response.data) {
-                return (response.data.access_token)
-            }
-            else {
-                console.log("error");
-            }
-        } catch (error) {
-            console.log("catch");
-        }
+  const [login, onChangeLogin] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  const image = {
+    uri: "https://images.unsplash.com/photo-1530176928500-2372a88e00b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fDNkJTIwYmFja2dyb3VuZCUyMHdoaXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  };
+  const getToken = async () => {
+    try {
+      const response = await axios.post(`https://api.intra.42.fr/oauth/token`, {
+        grant_type: "client_credentials",
+        client_id:
+          "160ec65318675dc7088c2c4b6139fbae91387ae8bcc71c363e0597676532bd70",
+        client_secret:
+          "21e6dfeba0b0cf4d69bd9d7516a1fafc3df090f4ba5e0538f9c6b3e258363ab0",
+      });
+      if (response.data) {
+        console.log(response.data.access_token);
+        return response.data.access_token;
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log("catch");
     }
-
-    const getUser = async (login) => {
-
-        if (login) {
-            try {
-                const token = await getToken();
-                const response = await axios.get(`https://api.intra.42.fr/v2/users/${login}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    }
-                );
-                if (response.data) {
-                    console.log(response.data)
-                    navigation.navigate('Infos', { userData: response.data })
-                }
-                else {
-                    console.log("error");
-                }
-            } catch (error) {
-                console.log(error);
-            }
+  };
+  const sendData = async (login, token) => {
+    try {
+      var response = await axios.get(
+        "https://api.intra.42.fr/v2/users/" + login.trim().toLowerCase(),
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
         }
-
+      );
+      console.log(response.data);
+      if (response.data) {
+        setLoading(false);
+        navigation.navigate("Infos", { userData: response.data });
+      }
+    } catch (error) {
+      console.log(error.message);
+      alert("login doesnt exist");
+      setLoading(false);
     }
+  };
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#F5FCFF',
-            flexDirection: 'column',
-        },
-        container1: {
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 1,
-            borderColor: "orange",
-            flexDirection: 'column',
-            padding: 15,
-            borderRadius: 10,
-            shadowColor: "#000",
-            shadowOffset: {
-                width: 0,
-                height: 6,
-            },
-            shadowOpacity: 0.39,
-            shadowRadius: 8.30,
+  const getUser = async (login) => {
+    setLoading(true);
+    login.trim();
+    if (login && login !== "") {
+      try {
+        var token = await AsyncStorage.getItem("access_token");
+        if (token) {
+          token = JSON.parse(token);
+          if (token.created_at + token.expires_in <= Date.now() / 1000) {
+            console.log("token expired");
+            token = await getToken();
+            if (token)
+              await AsyncStorage.setItem("access_token", JSON.stringify(token));
+          }
+        } else {
+          token = await getToken();
+          console.log(token);
+          if (token)
+            await AsyncStorage.setItem("access_token", JSON.stringify(token));
+        }
+        await sendData(login, token);
+      } catch (error) {
+        console.log(error);
+        alert(error);
+        setLoading(false);
+      }
+    } else alert("you should set a login first");
+    setLoading(false);
+  };
 
-            elevation: 13,
-        },
-        button: {
-            backgroundColor: "orange",
-            width: 100,
-            textAlign: 'center',
-            borderRadius: 10,
-            borderWidth: 1,
-            borderColor: "orange",
-            borderWidth: 1,
-            borderColor: "orange",
-            color: "red"
-        },
-        input: {
-            height: 40,
-            margin: 12,
-            borderWidth: 1,
-            borderColor: "orange",
-            padding: 10,
-            borderRadius: 5,
-        },
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+    },
+    viewContainer: {
+      marginTop: -100,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    tinyLogo: {
+      width: 150,
+      height: 150,
+      maxWidth: 200,
+      maxHeight: 200,
+      marginLeft: -20,
+    },
+    input: {
+      width: "80%",
+      margin: 12,
+      borderWidth: 1,
+      borderRadius: 5,
+      borderColor: "#EA7523",
+      backgroundColor: "white",
+      padding: 10,
+    },
+    button: {
+      backgroundColor: "#EA7523",
+      width: "80%",
+      padding: 10,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 5,
+    },
+    text: {
+      fontWeight: "bold",
+      color: "white",
+      fontSize: 17,
+    },
+  });
 
-    });
-
-    return (
-        <>
-            <View style={styles.container}>
-                <ImageBackground source={image} style={{
-                    width: '100%', height: '100%', justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-                    <View style={styles.container1}>
-
-                        {/* <Text style={{ color: "orange", fontWeight: "bold", fontSize: 30 }}>Home page</Text> */}
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={onChangeLogin}
-                            value={login}
-                            placeholder="Enter login"
-                        />
-                    </View>
-                    <Pressable style={styles.button} title="Search" onPress={() => getUser(login)}><Text style={{ color: "white", fontWeight: "bold" }}>Search</Text></Pressable>
-                </ImageBackground>
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          source={image}
+          style={{
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {!loading ? (
+            <View style={styles.viewContainer}>
+              <Image
+                style={styles.tinyLogo}
+                source={Home_image}
+                resizeMode="cover"
+              />
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeLogin}
+                value={login}
+                placeholder="Login"
+              />
+              <Pressable style={styles.button} onPress={() => getUser(login)}>
+                <Text style={styles.text}>Search</Text>
+              </Pressable>
             </View>
-        </>
-    )
+          ) : (
+            <ActivityIndicator size="large" color="#EA7523" />
+          )}
+        </ImageBackground>
+      </SafeAreaView>
+    </>
+  );
 }
--- components/Infos/profile.js
+++ components/Infos/profile.js
@@ -1,53 +1,158 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 import {
     StyleSheet,
     Text,
     View,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    ImageBackground,
+    SafeAreaView,
+    ScrollView
 } from 'react-native';
+import * as Progress from "react-native-progress";
+import defaultImage from "../../assets/default_img.png";
 
 export default function Profile({ route }) {
-      const { userData } = route.params;
+    const { userData } = route.params;
+    console.log(userData)
+    const [cursus, setCursus] = useState(userData.cursus_users[userData.cursus_users.length - 1]);
+    const [image, setImage] = useState(null)
 
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}></View>
-                <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
-                <View style={styles.body}>
-                    <View style={styles.bodyContent}>
-                        <Text style={styles.name}>John Doe</Text>
-                        <Text style={styles.info}>UX Designer / Mobile developer</Text>
-                        <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+    //check image 
+    function checkImage(url) {
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.send();
+        request.onload = function () {
+            if (request.status == 200) //if(statusText == OK)
+            {
+                setImage(url)
+            } else {
+                console.log("image doesn't exist");
+            }
+        }
+    }
+    useEffect(() => {
+        checkImage(userData.image_url);
+    }, [])
+    // cursus infos
+    const grade = cursus?.grade;
+    const cursus_skills = cursus?.skills;
+    const level = cursus?.level;
+    var level_per = 0;
+    if (level)
+        level_per = (level % 100) - parseInt(level);
+    return (
+        <SafeAreaView style={styles.container}>
 
-                        <TouchableOpacity style={styles.buttonContainer}>
-                            <Text>Opcion 1</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.buttonContainer}>
-                            <Text>Opcion 2</Text>
-                        </TouchableOpacity>
-                    </View>
+            <ImageBackground
+                style={styles.header}
+                // blurRadius={5}
+                source={{ uri: 'https://images.unsplash.com/photo-1530176928500-2372a88e00b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fDNkJTIwYmFja2dyb3VuZCUyMHdoaXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' }}
+            />
+
+            {image ? <Image style={styles.avatar} source={{ uri: `${image}` }} /> : <Image style={styles.avatar} source={{ uri: `${image}` }} />}
+            <View style={styles.body1}>
+                <Text style={styles.name}>{userData.login}</Text>
+                <Text style={styles.info}>{userData.usual_full_name}</Text>
+                <Text style={styles.info}>{userData.email}</Text>
+                <View style={{ marginTop: 20, alignItems: "center", justifyContent: 'center' }}>
+                    <Progress.Bar
+                        progress={level_per}
+                        width={300}
+                        color="#EA7523"
+                        height={20}
+                    >
+                        <Text
+                            style={{
+                                position: "absolute",
+                                color: "#000",
+                                fontSize: 11,
+                                marginLeft: 120,
+                                marginTop: 3
+                            }}
+                        >
+                            Level {level}%
+                        </Text>
+                    </Progress.Bar>
                 </View>
             </View>
-        );
-    
+            <ScrollView
+                style={styles.scroll}
+                contentContainerStyle={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <View style={styles.body}>
+                    <View style={styles.bodyContent}>
+
+                        {/* <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text> */}
+                        <View style={{ flexDirection: "column",  marginBottom: 30 }}>
+                            {cursus_skills?.map((item, i) => {
+                                return (
+                                    <View key={item.id} style={{  marginBottom: 5 }}>
+                                        <View style={{ flexDirection: 'column' }}>
+                                            <Text
+                                                style={{
+                                                    color: "black",
+                                                    textAlign: "center",
+                                                    marginBottom: 4,
+                                                }}
+                                            >
+                                                {item.name}
+                                            </Text>
+                                            <Text style={{
+                                                fontSize: 12,
+                                                fontWeight: "bold",
+                                                color: "#EA7523",
+                                                alignItems: "flex-end",
+                                                marginBottom: 2
+                                            }}>{item.level.toFixed(2)}%</Text>
+                                        </View>
+                                        {/* <ProgressBar
+                                            progress={item.level.toFixed(2) / 100}
+                                            color="#EA7523"
+                                            style={{ width: 300, height: 5 }}
+                                        /> */}
+                                    </View>
+                                );
+                            })}
+                        </View>
+
+                        {/* <TouchableOpacity style={styles.buttonContainer}>
+                        <Text>Opcion 1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonContainer}>
+                        <Text>Opcion 2</Text>
+                    </TouchableOpacity> */}
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView >
+    );
+
 }
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: "#00BFFF",
+        // backgroundColor: "#00BFFF",
         height: 200,
     },
     avatar: {
         width: 130,
         height: 130,
         borderRadius: 63,
-        borderWidth: 4,
+        borderWidth: 1,
         borderColor: "white",
         marginBottom: 10,
         alignSelf: 'center',
         position: 'absolute',
         marginTop: 130
+    },
+    headerBackgroundImage: {
+        paddingBottom: 20,
+        paddingTop: 45,
     },
     name: {
         fontSize: 22,
@@ -55,12 +160,19 @@ const styles = StyleSheet.create({
         fontWeight: '600',
     },
     body: {
-        marginTop: 40,
+        margin: 5,
+        marginBottom: 350,
+    },
+    body1: {
+        marginTop: 60,
+        alignSelf: 'center',
+        alignItems: 'center',
     },
     bodyContent: {
         flex: 1,
         alignItems: 'center',
         padding: 30,
+
     },
     name: {
         fontSize: 28,
@@ -68,8 +180,8 @@ const styles = StyleSheet.create({
         fontWeight: "600"
     },
     info: {
-        fontSize: 16,
-        color: "#00BFFF",
+        fontSize: 13,
+        color: "#696969",
         marginTop: 10
     },
     description: {
@@ -88,5 +200,9 @@ const styles = StyleSheet.create({
         width: 250,
         borderRadius: 30,
         backgroundColor: "#00BFFF",
+    },
+    scroll: {
+        width: "100%",
+        height: "100%",
     },
 });
